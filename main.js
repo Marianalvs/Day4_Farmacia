325aab564126ec4c57cd9e4a91c694a09e402d19
Mariana Alves <mariana.lves@hotmail.com>
2023-03-27 21:30:09 -0300

    Atualizaçao de HTML e CSS. Adicionado JavaScript

--- main.js
+++ main.js
@@ -1,41 +1,90 @@
-let medicamentos = [];
+var Medicamento = [];
 
-function Cadastrar(){
-
+function Cadastrar() {
     event.preventDefault();
+    let newMed = {
+        "medName": document.getElementById("fname").value,
+        "medQtd": document.getElementById("fqty").value,
+        "medPrice": document.getElementById("fprice").value,
+        "medClass": document.getElementById("fclass").value,
+    }
+    
+    resetForm();
 
-    let medicamento = {
-        "medName":document.getElementById('fname').value,
-        "medQty":document.getElementById('fqty').value,
-        "medPrice":document.getElementById('fprice').value,
-        "medClass":document.getElementById('fclass').value,
+
+    if(Validar(newMed) == true){
+        Medicamento.push(newMed)
+    }
+      
+    renderTable();
+}
+
+function Validar(newMed){
+    let msg = '';
+
+        if(newMed.medName == ''){
+            msg += 'Por favor, insira corretamente o nome do Medicamento! \n'
+        }
+
+        if(newMed.medQtd == ''){
+            msg += 'Por favor, insira corretamente a quantidade do Medicamento! \n'
+        }
+
+        if(newMed.medPrice == ''){
+            msg += 'Por favor, insira corretamente o preço do Medicamento! \n'
+        }
+
+        if(newMed.medClass == ''){
+            msg += 'Por favor, insira corretamente a Classe do Medicamento! \n'
+        }
+        if(msg != ''){
+            alert(msg)
+            return false
+        }
+        return true
+
+
+    
+}
+
+
+function removeMed(index){
+    Medicamento.splice(index,1);
+    renderTable();
+}
+
+function renderTable(){
+    let table = document.getElementById("dataTable");
+    table.innerHTML =  `<th>ID</th>
+    <th>Medicamento</th>
+    <th>Quantidade</th>
+    <th>Preço Unit.</th>
+    <th>Classe de Medicamento</th>
+    <th>Remover</th>`;
+
+
+    for(let i = 1; i < Medicamento.length; i++)
+    {
+
+        
+        table.innerHTML = table.innerHTML  +  `<tr>
+        <td>${i}</td>
+        <td>${Medicamento[i].medName}</td>
+        <td>${Medicamento[i].medQtd}</td>
+        <td>${Medicamento[i].medPrice}</td>
+        <td>${Medicamento[i].medClass}</td>
+        <td><img src="remove.png" width="25px" height="25px" onclick="removeMed(${i})"></td>
+    </tr>`
     }
 
+}
+
+function resetForm(){
     document.getElementById("fname").value = "";
     document.getElementById("fqty").value = "";
     document.getElementById("fprice").value = "";
     document.getElementById("fclass").value = "";
-
-    medicamentos.push(medicamento)
-
-    function renderTable(){
-
-        let card = document.getElementById("dataCard");
-        card.innerHTML =  `<th>ID</th>
-        <th>Medicamento</th>
-        <th>Quantidade</th>
-        <th>Classe de Medicamento</th>
-        <th>Remover</th>`;
-    
-        card.innerHTML = card.innerHTML  +  `<tr>
-            <td>${i}</td>
-            <td>${meds[i].medName}</td>
-            <td>${meds[i].medQtd}</td>
-            <td>${meds[i].medClass}</td>
-            <td><img src="remove.png" width="25px" height="25px" onclick="removeMed(${i})"></td>
-        </tr>`
-    } 
-      
-
-
 }
+
+renderTable()
+
